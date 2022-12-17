1c0c19f57171181cf235d6816bc1fbfcdbd79192
Jason N <fireworkinyourbathtub@gmail.com>
2022-12-17 16:49:33 -0500

    add prototype display program

---
+++ src/circuit.rs
@@ -0,0 +1,36 @@
+pub struct Circuit {
+    pub arity: usize,
+    pub gates: Vec<Gate>,
+    pub output: Vec<Value>,
+}
+
+pub enum Gate {
+    // Custom(Circuit, Vec<Value>),
+    And(Value, Value),
+}
+
+impl Gate {
+    pub fn inputs(&self) -> Vec<Value> {
+        match self {
+            Gate::And(a, b) => vec![*a, *b],
+        }
+    }
+
+    pub fn num_inputs(&self) -> usize {
+        match self {
+            Gate::And(_, _) => 2,
+        }
+    }
+
+    pub fn num_outputs(&self) -> usize {
+        match self {
+            Gate::And(_, _) => 1,
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+pub enum Value {
+    Arg(usize),
+    GateValue(usize, usize),
+}
