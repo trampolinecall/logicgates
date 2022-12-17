fd354874a4f85214a126a914476e2e08f6dc9511
Jason N <fireworkinyourbathtub@gmail.com>
2022-12-17 13:43:50 -0500

    add table function

--- src/bin/thing.rs
+++ src/bin/thing.rs
@@ -1,12 +1,9 @@
-use logicgates::rep;
 use logicgates::eval;
+use logicgates::rep;
 
-fn main () {
-    let main_circuit = rep::Circuit {
-        arity: 2,
-        gates: vec![rep::Gate::And(rep::Value::Arg(0), rep::Value::Arg(1))],
-        output: vec![rep::Value::Register(0), rep::Value::Register(0)]
-    };
+fn main() {
+    let main_circuit = rep::Circuit { arity: 2, gates: vec![rep::Gate::And(rep::Value::Arg(0), rep::Value::Arg(1))], output: vec![rep::Value::Register(0)] };
 
-    println!("{:?}", eval::eval(&main_circuit, &vec![true, true]))
+    println!("{:?}", eval::eval(&main_circuit, &vec![true, true]));
+    println!("{:#?}", eval::table(&main_circuit));
 }
