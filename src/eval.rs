f5dd1eb83388105344f79516564cfb31d287f2e2
trampolinecall <>
2022-12-17 13:43:50 -0500

    add table function

--- src/eval.rs
+++ src/eval.rs
@@ -1,7 +1,31 @@
+use std::collections::HashMap;
+
 use crate::rep;
 
+pub fn table(circuit: &rep::Circuit) -> HashMap<Vec<bool>, Vec<bool>> {
+    let mut inputs: Vec<Vec<bool>> = vec![vec![false], vec![true]];
+    for _ in 0..(circuit.arity - 1) {
+        let mut inputs_false = inputs.clone();
+        let mut inputs_true = inputs;
+
+        inputs_false.iter_mut().for_each(|i| i.push(false));
+        inputs_true.iter_mut().for_each(|i| i.push(true));
+
+        inputs = inputs_false;
+        inputs.extend(inputs_true);
+    }
+
+    inputs
+        .into_iter()
+        .map(|input| {
+            let res = eval(circuit, &input);
+            (input, res)
+        })
+        .collect()
+}
+
 pub fn eval(circuit: &rep::Circuit, args: &[bool]) -> Vec<bool> {
-    assert!(args.len() == circuit.arity);
+    assert_eq!(args.len(), circuit.arity);
 
     let mut registers = Vec::new();
 
@@ -13,7 +37,6 @@ pub fn eval(circuit: &rep::Circuit, args: &[bool]) -> Vec<bool> {
                 registers.extend(eval(subcircuit, &sub_args))
             }
             */
-
             rep::Gate::And(a, b) => registers.push(get_value(a, &args, &registers) && get_value(b, &args, &registers)),
         };
     }
