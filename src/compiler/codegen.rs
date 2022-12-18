469b669e27a9a15a027dfef2a62410864f100f71
Jason N <fireworkinyourbathtub@gmail.com>
2022-12-18 16:33:53 -0500

    make parser return hashmap

--- src/compiler/codegen.rs
+++ src/compiler/codegen.rs
@@ -1,5 +1,6 @@
+use std::collections::HashMap;
 use super::parser::ast;
 
-pub(crate) fn compile(ast: Vec<ast::Gate>) -> Option<crate::circuit::Circuit> {
+pub(crate) fn compile(ast: HashMap<&str, ast::Gate>) -> Option<crate::circuit::Circuit> {
     todo!()
 }
