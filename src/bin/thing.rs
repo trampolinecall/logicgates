e7905d533b81418f16797967a6f7ceaed0cd0fa3
trampolinecall <>
2022-12-17 19:59:59 -0500

    run clippy and fmt

--- src/bin/thing.rs
+++ src/bin/thing.rs
@@ -3,6 +3,6 @@ use logicgates::circuit;
 fn main() {
     let main_circuit = circuit::Circuit { arity: 2, gates: vec![circuit::Gate::And(circuit::Value::Arg(0), circuit::Value::Arg(1))], output: vec![circuit::Value::GateValue(0, 0)] };
 
-    println!("{:?}", main_circuit.eval(&vec![true, true]));
+    println!("{:?}", main_circuit.eval(&[true, true]));
     println!("{:#?}", main_circuit.table());
 }
