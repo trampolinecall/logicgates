ccd2b7e83902f47584c5e6d16084eeed7cab75d0
trampolinecall <>
2022-12-18 17:46:32 -0500

    refactor state into big state structs

--- src/compiler/circuitgen.rs
+++ src/compiler/circuitgen.rs
@@ -13,6 +13,23 @@ enum CircuitGenError<'file> {
     NoSuchCircuit(&'file str),
 }
 
+#[derive(Default)]
+struct GlobalGenState<'file> {
+    circuit_table: HashMap<&'file str, circuit::Circuit>,
+}
+
+#[derive(Default)]
+struct CircuitGenState<'file> {
+    locals: HashMap<&'file str, Vec<circuit::Value>>,
+    gates: Vec<circuit::Gate>,
+}
+impl CircuitGenState<'_> {
+    fn add_gate(&mut self, val: circuit::Gate) -> usize {
+        self.gates.push(val);
+        self.gates.len() - 1
+    }
+}
+
 impl From<CircuitGenError<'_>> for CompileError {
     fn from(val: CircuitGenError) -> Self {
         match val {
@@ -25,56 +42,45 @@ impl From<CircuitGenError<'_>> for CompileError {
 }
 
 pub(crate) fn generate(ast: Vec<ast::Circuit>) -> Option<circuit::Circuit> {
-    let mut circuit_name_table = HashMap::new();
+    let mut global_state = GlobalGenState::default();
 
     for circuit in ast {
-        let (name, circuit) = convert_circuit(&circuit_name_table, circuit)?; // TODO: report multiple errors from this
-        if circuit_name_table.contains_key(name) {
+        let (name, circuit) = convert_circuit(&global_state, circuit)?; // TODO: report multiple errors from this
+        if global_state.circuit_table.contains_key(name) {
             CircuitGenError::Duplicate(name).report();
             None?
         } else {
-            circuit_name_table.insert(name, circuit);
+            global_state.circuit_table.insert(name, circuit);
         }
     }
     todo!()
 }
 
-fn convert_circuit<'file>(circuit_name_table: &HashMap<&'file str, circuit::Circuit>, circuit_ast: ast::Circuit<'file>) -> Option<(&'file str, circuit::Circuit)> {
+fn convert_circuit<'file>(global_state: &GlobalGenState, circuit_ast: ast::Circuit<'file>) -> Option<(&'file str, circuit::Circuit)> {
     let name = circuit_ast.name;
 
-    let mut locals: HashMap<&str, Vec<circuit::Value>> = HashMap::new();
-    let mut gates: Vec<circuit::Gate> = Vec::new();
+    let mut circuit_state = CircuitGenState::default();
     for r#let in circuit_ast.lets {
-        let result = convert_expr(circuit_name_table, &mut gates, &mut locals, r#let.val);
+        let result = convert_expr(global_state, &mut circuit_state, r#let.val);
 
         // TODO: assign results to locals
     }
 
-    let outputs = convert_expr(circuit_name_table, &mut gates, &mut locals, circuit_ast.outputs)?;
+    let outputs = convert_expr(global_state, &mut circuit_state, circuit_ast.outputs)?;
 
-    Some((name, circuit::Circuit { name: name.into(), num_inputs: pattern_size(&circuit_ast.inputs), gates, outputs }))
+    Some((name, circuit::Circuit { name: name.into(), num_inputs: pattern_size(&circuit_ast.inputs), gates: circuit_state.gates, outputs }))
 }
 
-fn convert_expr<'file>(
-    circuit_name_table: &HashMap<&'file str, circuit::Circuit>,
-    gates: &mut Vec<circuit::Gate>,
-    locals: &mut HashMap<&str, Vec<circuit::Value>>,
-    exprs: Vec<ast::Expr>,
-) -> Option<Vec<circuit::Value>> {
-    let results = exprs.into_iter().map(|e| convert_single_expr(circuit_name_table, gates, locals, e));
+fn convert_expr<'file>(global_state: &GlobalGenState, circuit_state: &mut CircuitGenState, exprs: Vec<ast::Expr>) -> Option<Vec<circuit::Value>> {
+    let results = exprs.into_iter().map(|e| convert_single_expr(global_state, circuit_state, e));
     let results_no_none: Option<_> = results.collect::<Option<Vec<Vec<circuit::Value>>>>(); // TODO: dont stop at the first one in order to report all the errors
     Some(results_no_none?.into_iter().flatten().collect::<Vec<circuit::Value>>())
 }
 
-fn convert_single_expr<'file>(
-    circuit_name_table: &HashMap<&'file str, circuit::Circuit>,
-    gates: &mut Vec<circuit::Gate>,
-    locals: &mut HashMap<&str, Vec<circuit::Value>>,
-    expr: ast::Expr,
-) -> Option<Vec<circuit::Value>> {
+fn convert_single_expr<'file>(global_state: &GlobalGenState, circuit_state: &mut CircuitGenState, expr: ast::Expr) -> Option<Vec<circuit::Value>> {
     match expr {
         ast::Expr::Ref(name, slots) => {
-            let name_resolved = match locals.get(name) {
+            let name_resolved = match circuit_state.locals.get(name) {
                 Some(resolved) => resolved,
                 None => {
                     CircuitGenError::NoSuchLocal(name).report();
@@ -96,7 +102,7 @@ fn convert_single_expr<'file>(
         }
 
         ast::Expr::Call(circuit_name, inputs) => {
-            let name_resolved = match circuit_name_table.get(circuit_name) {
+            let name_resolved = match global_state.circuit_table.get(circuit_name) {
                 Some(n) => n,
                 None => {
                     CircuitGenError::NoSuchCircuit(circuit_name).report();
@@ -104,21 +110,16 @@ fn convert_single_expr<'file>(
                 }
             };
 
-            let inputs = convert_expr(circuit_name_table, gates, locals, inputs)?;
-            let gate = add_gate(gates, circuit::Gate::Custom(name_resolved.clone(), inputs));
+            let inputs = convert_expr(global_state, circuit_state, inputs)?;
+            let gate = circuit_state.add_gate(circuit::Gate::Custom(name_resolved.clone(), inputs));
             Some((0..name_resolved.outputs.len()).map(|i| circuit::Value::GateValue(gate, i)).collect())
         }
 
         ast::Expr::Const(val) => {
-            let gate_i = add_gate(gates, circuit::Gate::Const(val));
+            let gate_i = circuit_state.add_gate(circuit::Gate::Const(val));
             Some(vec![circuit::Value::GateValue(gate_i, 0)])
         }
     }
-}
-
-fn add_gate(gates: &mut Vec<circuit::Gate>, val: circuit::Gate) -> usize {
-    gates.push(val);
-    gates.len() - 1
 }
 
 fn pattern_size(arguments: &[ast::Pattern]) -> usize {
