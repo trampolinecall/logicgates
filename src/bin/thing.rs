afbeb06615fadc96da72c56d62452f7b56563e9d
Jason N <fireworkinyourbathtub@gmail.com>
2022-12-18 19:06:03 -0500

    make and take any number of arguments

--- src/bin/thing.rs
+++ src/bin/thing.rs
@@ -1,7 +1,8 @@
 use logicgates::circuit;
 
 fn main() {
-    let main_circuit = circuit::Circuit { name: "thing".into(), num_inputs: 2, gates: vec![circuit::Gate::And(circuit::Value::Arg(0), circuit::Value::Arg(1))], outputs: vec![circuit::Value::GateValue(0, 0)] };
+    let main_circuit =
+        circuit::Circuit { name: "thing".into(), num_inputs: 2, gates: vec![circuit::Gate::And(vec![circuit::Value::Arg(0), circuit::Value::Arg(1)])], outputs: vec![circuit::Value::GateValue(0, 0)] };
 
     println!("{:?}", main_circuit.eval(&[true, true]));
     println!("{:#?}", main_circuit.table());
