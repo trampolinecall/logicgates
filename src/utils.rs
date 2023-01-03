a58cd517fae52a8976343060be004bc00d8165cd
trampolinecall <>
2023-01-03 00:46:19 -0500

    add utility CollectAll trait

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,52 @@
+// TODO: there is probably a better way of doing this
+pub(crate) trait CanCollectAll { // TODO: figure out how to make this private
+    type Result;
+    fn collect_all(iter: impl Iterator<Item = Self>) -> Self::Result;
+}
+
+pub(crate) trait CollectAll {
+    type Result;
+    fn collect_all(self) -> Self::Result;
+}
+
+impl<I: CanCollectAll, T: Iterator<Item = I>> CollectAll for T {
+    type Result = I::Result;
+
+    fn collect_all(self) -> Self::Result {
+        CanCollectAll::collect_all(self)
+    }
+}
+
+impl<T> CanCollectAll for Option<T> {
+    type Result = Option<Vec<T>>;
+
+    fn collect_all(iter: impl Iterator<Item = Self>) -> Self::Result {
+        // collect into a vec first to evaluate all of the things and then collect all of the results to stop at the frist error
+        iter.collect::<Vec<_>>().into_iter().collect()
+    }
+}
+
+impl<R, E> CanCollectAll for Result<R, E> {
+    type Result = Result<Vec<R>, Vec<E>>;
+
+    fn collect_all(iter: impl Iterator<Item = Self>) -> Self::Result {
+        let mut results = Vec::new();
+        let mut errors = Vec::new();
+        for item in iter {
+            match item {
+                Ok(o) => results.push(o),
+                Err(e) => errors.push(e),
+            }
+        }
+
+        if errors.is_empty() {
+            Ok(results)
+        } else {
+            Err(errors)
+        }
+    }
+}
+
 pub(crate) fn enumerate_inputs(arity: usize) -> Vec<Vec<bool>> {
     let mut inputs = vec![vec![]];
     for _ in 0..arity {
