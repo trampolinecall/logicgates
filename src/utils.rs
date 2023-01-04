4a4d3af69c405c498a29763035de7173bd0dcb18
trampolinecall <>
2023-01-03 23:31:37 -0500

    make CanCollectAll work for any FromIterator type

--- src/utils.rs
+++ src/utils.rs
@@ -1,35 +1,43 @@
 // TODO: there is probably a better way of doing this
-pub(crate) trait CanCollectAll { // TODO: figure out how to make this private
-    type Result;
-    fn collect_all(iter: impl Iterator<Item = Self>) -> Self::Result;
+pub(crate) trait CanCollectAll {
+    // TODO: figure out how to make this private
+    type Output<Collection>;
+    type Item;
+    fn collect_all<F: FromIterator<Self::Item>>(iter: impl Iterator<Item = Self>) -> Self::Output<F>;
 }
 
 pub(crate) trait CollectAll {
-    type Result;
-    fn collect_all(self) -> Self::Result;
+    type Item;
+    type Output<Collection>;
+    fn collect_all<F: FromIterator<Self::Item>>(self) -> Self::Output<F>;
 }
 
 impl<I: CanCollectAll, T: Iterator<Item = I>> CollectAll for T {
-    type Result = I::Result;
+    type Item = <I as CanCollectAll>::Item;
+    type Output<Collection> = I::Output<Collection>;
 
-    fn collect_all(self) -> Self::Result {
+    fn collect_all<F: FromIterator<Self::Item>>(self) -> Self::Output<F> {
         CanCollectAll::collect_all(self)
     }
 }
 
 impl<T> CanCollectAll for Option<T> {
-    type Result = Option<Vec<T>>;
+    type Output<Collection> = Option<Collection>;
+    type Item = T;
 
-    fn collect_all(iter: impl Iterator<Item = Self>) -> Self::Result {
+    fn collect_all<F: FromIterator<Self::Item>>(iter: impl Iterator<Item = Self>) -> Self::Output<F> {
         // collect into a vec first to evaluate all of the things and then collect all of the results to stop at the frist error
         iter.collect::<Vec<_>>().into_iter().collect()
     }
 }
 
 impl<R, E> CanCollectAll for Result<R, E> {
-    type Result = Result<Vec<R>, Vec<E>>;
+    type Output<Collection> = Result<Collection, Vec<E>>;
 
-    fn collect_all(iter: impl Iterator<Item = Self>) -> Self::Result {
+    type Item = R;
+
+    fn collect_all<F: std::iter::FromIterator<R>>(iter: impl Iterator<Item = Self>) -> Self::Output<F> {
+        // TODO: make a better implementation of this
         let mut results = Vec::new();
         let mut errors = Vec::new();
         for item in iter {
@@ -40,7 +48,7 @@ impl<R, E> CanCollectAll for Result<R, E> {
         }
 
         if errors.is_empty() {
-            Ok(results)
+            Ok(results.into_iter().collect())
         } else {
             Err(errors)
         }
