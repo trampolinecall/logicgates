b558012fcb89afea5badd51eb0ae940c1f7942ea
trampolinecall <>
2023-01-08 18:34:54 -0500

    implement better error reporting

--- src/compiler/error.rs
+++ src/compiler/error.rs
@@ -6,11 +6,17 @@ pub(crate) use span::{File, Span};
 pub(crate) struct CompileError<'file> {
     span: Span<'file>,
     message: String,
+    notes: Vec<(Span<'file>, String)>,
 }
 
 impl<'file> CompileError<'file> {
     pub(crate) fn new(span: Span<'file>, message: String) -> Self {
-        Self { span, message }
+        Self { span, message, notes: Vec::new() }
+    }
+
+    pub(crate) fn note(mut self, sp: Span<'file>, note: String) -> Self {
+        self.notes.push((sp, note));
+        self
     }
 }
 
@@ -25,6 +31,23 @@ impl<'a, T: Into<CompileError<'a>>> Report for T {
 }
 
 fn report(e: CompileError) {
-    // TODO: better error reporting
-    eprintln!("error at {}: {}", e.span, e.message);
+    print_message(&format!("error at {}: {}", e.span, e.message), e.span);
+
+    for (sp, note) in e.notes {
+        print_message(&format!("  - {}", note), sp);
+    }
+}
+
+fn print_message(message: &str, span: Span) {
+    let (start_line_nr, start_col) = span::get_lc(span.0, span.1);
+    let (end_line_nr, end_col) = span::get_lc(span.0, span.2);
+    let line_quote = span.0.contents.lines().nth(start_line_nr - 1).unwrap_or("");
+
+    eprintln!("{}", message);
+    eprintln!("{}", line_quote);
+    eprint!("{}{}", " ".repeat(start_col - 1), "^".repeat(end_col - start_col));
+    if start_line_nr != end_line_nr {
+        eprint!("...")
+    }
+    eprintln!()
 }
