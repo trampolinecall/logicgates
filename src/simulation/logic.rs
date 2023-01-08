b36468e260ed8ae7b9a9f1dabd4247019fa2c74c
trampolinecall <>
2023-01-08 16:17:23 -0500

    reorganize logic module

--- src/simulation/logic.rs
+++ src/simulation/logic.rs
@@ -37,6 +37,35 @@ pub(crate) struct CircuitInputNodeIdx(pub(crate) CircuitIndex, pub(crate) usize,
 #[derive(Clone, Copy, Hash, PartialEq, Eq, Debug)]
 pub(crate) struct CircuitOutputNodeIdx(pub(crate) CircuitIndex, pub(crate) usize, ());
 
+impl From<GateOutputNodeIdx> for NodeIdx {
+    fn from(v: GateOutputNodeIdx) -> Self {
+        Self::GO(v)
+    }
+}
+impl From<CircuitInputNodeIdx> for NodeIdx {
+    fn from(v: CircuitInputNodeIdx) -> Self {
+        Self::CI(v)
+    }
+}
+impl From<GateInputNodeIdx> for NodeIdx {
+    fn from(v: GateInputNodeIdx) -> Self {
+        Self::GI(v)
+    }
+}
+impl From<CircuitOutputNodeIdx> for NodeIdx {
+    fn from(v: CircuitOutputNodeIdx) -> Self {
+        Self::CO(v)
+    }
+}
+
+#[derive(Clone, Copy, Hash, PartialEq, Eq, Debug)]
+pub(crate) enum NodeIdx {
+    CI(CircuitInputNodeIdx),
+    CO(CircuitOutputNodeIdx),
+    GI(GateInputNodeIdx),
+    GO(GateOutputNodeIdx),
+}
+
 impl Calculation {
     // default value for the outputs is whatever value results from having all false inputs
     pub(crate) fn new_nand(index: GateIndex) -> Calculation {
@@ -75,42 +104,107 @@ impl Node {
     }
 }
 
-#[derive(Clone, Copy, Hash, PartialEq, Eq, Debug)]
-pub(crate) enum NodeIdx {
-    CI(CircuitInputNodeIdx),
-    CO(CircuitOutputNodeIdx),
-    GI(GateInputNodeIdx),
-    GO(GateOutputNodeIdx),
-}
-
-impl From<GateOutputNodeIdx> for NodeIdx {
-    fn from(v: GateOutputNodeIdx) -> Self {
-        Self::GO(v)
+// inputs and outputs {{{1
+pub(crate) fn gate_inputs<'a: 'c, 'b: 'c, 'c>(circuits: &'a CircuitMap, gates: &'b GateMap, gate: GateIndex) -> &'c [Node] {
+    let gate = &gates[gate];
+    match &gate.calculation.kind {
+        CalculationKind::Nand(i, _) => i,
+        CalculationKind::Const(i, _) => i,
+        CalculationKind::Custom(circuit_idx) => &circuits[*circuit_idx].inputs,
     }
 }
-impl From<CircuitInputNodeIdx> for NodeIdx {
-    fn from(v: CircuitInputNodeIdx) -> Self {
-        Self::CI(v)
-    }
-}
-impl From<GateInputNodeIdx> for NodeIdx {
-    fn from(v: GateInputNodeIdx) -> Self {
-        Self::GI(v)
-    }
-}
-impl From<CircuitOutputNodeIdx> for NodeIdx {
-    fn from(v: CircuitOutputNodeIdx) -> Self {
-        Self::CO(v)
+pub(crate) fn gate_outputs<'a: 'c, 'b: 'c, 'c>(circuits: &'a CircuitMap, gates: &'b GateMap, gate: GateIndex) -> &'c [Node] {
+    let gate = &gates[gate];
+    match &gate.calculation.kind {
+        CalculationKind::Nand(_, o) | CalculationKind::Const(_, o) => o,
+        CalculationKind::Custom(circuit_idx) => &circuits[*circuit_idx].outputs,
     }
 }
 
+pub(crate) fn gate_inputs_mut<'a: 'c, 'b: 'c, 'c>(circuits: &'a mut CircuitMap, gates: &'b mut GateMap, gate: GateIndex) -> &'c mut [Node] {
+    let gate = &mut gates[gate];
+    match &mut gate.calculation.kind {
+        CalculationKind::Nand(i, _) => i,
+        CalculationKind::Const(i, _) => i,
+        CalculationKind::Custom(circuit_idx) => &mut circuits[*circuit_idx].inputs,
+    }
+}
+pub(crate) fn gate_outputs_mut<'a: 'c, 'b: 'c, 'c>(circuits: &'a mut CircuitMap, gates: &'b mut GateMap, gate: GateIndex) -> &'c mut [Node] {
+    let gate = &mut gates[gate];
+    match &mut gate.calculation.kind {
+        CalculationKind::Nand(_, o) | CalculationKind::Const(_, o) => o,
+        CalculationKind::Custom(circuit_idx) => &mut circuits[*circuit_idx].outputs,
+    }
+}
+
+pub(crate) fn gate_num_inputs(circuits: &CircuitMap, gates: &GateMap, gate: GateIndex) -> usize {
+    gate_inputs(circuits, gates, gate).len()
+}
+pub(crate) fn gate_num_outputs(circuits: &CircuitMap, gates: &GateMap, gate: GateIndex) -> usize {
+    gate_outputs(circuits, gates, gate).len()
+}
+
+// enumerating indexes {{{2
 pub(crate) fn circuit_input_indexes(circuit: &Circuit) -> impl Iterator<Item = CircuitInputNodeIdx> + '_ {
     (0..circuit.inputs.len()).map(|i| CircuitInputNodeIdx(circuit.index, i, ()))
 }
 pub(crate) fn circuit_output_indexes(circuit: &Circuit) -> impl Iterator<Item = CircuitOutputNodeIdx> + '_ {
     (0..circuit.outputs.len()).map(|i| CircuitOutputNodeIdx(circuit.index, i, ()))
 }
+pub(crate) fn gate_input_indexes(circuits: &CircuitMap, gates: &GateMap, gate: GateIndex) -> impl ExactSizeIterator<Item = GateInputNodeIdx> {
+    (0..gate_num_inputs(circuits, gates, gate)).map(move |i| GateInputNodeIdx(gate, i, ()))
+    // a bit strange because GateIndex is Copy so it really shouldnt have to be moved (?)
+}
+pub(crate) fn gate_output_indexes(circuits: &CircuitMap, gates: &GateMap, gate: GateIndex) -> impl ExactSizeIterator<Item = GateOutputNodeIdx> {
+    (0..gate_num_outputs(circuits, gates, gate)).map(move |i| GateOutputNodeIdx(gate, i, ()))
+}
+// getting nodes {{{1
+pub(crate) fn gate_get_input<'a: 'c, 'b: 'c, 'c>(circuits: &'a CircuitMap, gates: &'b GateMap, index: GateInputNodeIdx) -> &'c Node {
+    // let name = gates[index.0].name();
+    let inputs = gate_inputs(circuits, gates, index.0);
+    // inputs.get(index.1).unwrap_or_else(|| panic!("gate input node index invalid: index has index {} but '{}' gate has only {} inputs", index.1, name, inputs.len()))
+    inputs.get(index.1).unwrap()
+}
+pub(crate) fn gate_get_input_mut<'a: 'c, 'b: 'c, 'c>(circuits: &'a mut CircuitMap, gates: &'b mut GateMap, index: GateInputNodeIdx) -> &'c mut Node {
+    // let name = gates[index.0].name();
+    let inputs = gate_inputs_mut(circuits, gates, index.0);
+    // let len = inputs.len();
+    // inputs.get_mut(index.1).unwrap_or_else(|| panic!("gate input node index invalid: index has index {} but '{}' gate has only {} inputs", index.1, name, len))
+    inputs.get_mut(index.1).unwrap()
+    // TODO: there is probably a better way of doing this that doesnt need this code to be copy pasted
+    // TODO: there is also probably a better way of doing this that doesnt need
+}
+pub(crate) fn gate_get_output<'a: 'c, 'b: 'c, 'c>(circuits: &'a CircuitMap, gates: &'b GateMap, index: GateOutputNodeIdx) -> &'c Node {
+    // let name = gates[index.0].name();
+    let outputs = gate_outputs(circuits, gates, index.0);
+    // outputs.get(index.1).unwrap_or_else(|| panic!("gate output node index invalid: index has index {} but '{}' gate has only {} outputs", index.1, name, outputs.len()))
+    outputs.get(index.1).unwrap()
+}
+pub(crate) fn gate_get_output_mut<'a: 'c, 'b: 'c, 'c>(circuits: &'a mut CircuitMap, gates: &'b mut GateMap, index: GateOutputNodeIdx) -> &'c mut Node {
+    // let name = gates[index.0].name();
+    let outputs = gate_outputs_mut(circuits, gates, index.0);
+    // let len = outputs.len();
+    // outputs.get_mut(index.1).unwrap_or_else(|| panic!("gate output node index invalid: index has index {} but '{}' gate has only {} outputs", index.1, name, len))
+    outputs.get_mut(index.1).unwrap()
+}
 
+pub(crate) fn get_node<'a: 'c, 'b: 'c, 'c>(circuits: &'a CircuitMap, gates: &'b GateMap, index: NodeIdx) -> &'c Node {
+    match index {
+        NodeIdx::CI(ci) => &circuits[ci.0].inputs[ci.1],
+        NodeIdx::CO(co) => &circuits[co.0].outputs[co.1],
+        NodeIdx::GI(gi) => gate_get_input(circuits, gates, gi),
+        NodeIdx::GO(go) => gate_get_output(circuits, gates, go),
+    }
+}
+pub(crate) fn get_node_mut<'a: 'c, 'b: 'c, 'c>(circuits: &'a mut CircuitMap, gates: &'b mut GateMap, index: NodeIdx) -> &'c mut Node {
+    match index {
+        NodeIdx::CI(ci) => &mut circuits[ci.0].inputs[ci.1],
+        NodeIdx::CO(co) => &mut circuits[co.0].outputs[co.1],
+        NodeIdx::GI(gi) => gate_get_input_mut(circuits, gates, gi),
+        NodeIdx::GO(go) => gate_get_output_mut(circuits, gates, go),
+    }
+}
+// node values {{{1
 // TODO: test connection, replacing old connection, also rename to set_node_value_passthrough or something like that
 pub(crate) fn connect(circuits: &mut CircuitMap, gates: &mut GateMap, producer_idx: NodeIdx, receiver_idx: NodeIdx) {
     set_node_value(circuits, gates, receiver_idx, Value::Passthrough(producer_idx))
@@ -153,24 +247,7 @@ pub(crate) fn toggle_input(circuits: &mut CircuitMap, gates: &mut GateMap, circu
 pub(crate) fn set_input(circuits: &mut CircuitMap, gates: &mut GateMap, ci: CircuitInputNodeIdx, value: bool) {
     set_node_value(circuits, gates, ci.into(), Value::Manual(value));
 }
-
-pub(crate) fn get_node<'a: 'c, 'b: 'c, 'c>(circuits: &'a CircuitMap, gates: &'b GateMap, index: NodeIdx) -> &'c Node {
-    match index {
-        NodeIdx::CI(ci) => &circuits[ci.0].inputs[ci.1],
-        NodeIdx::CO(co) => &circuits[co.0].outputs[co.1],
-        NodeIdx::GI(gi) => gate_get_input(circuits, gates, gi),
-        NodeIdx::GO(go) => gate_get_output(circuits, gates, go),
-    }
-}
-pub(crate) fn get_node_mut<'a: 'c, 'b: 'c, 'c>(circuits: &'a mut CircuitMap, gates: &'b mut GateMap, index: NodeIdx) -> &'c mut Node {
-    match index {
-        NodeIdx::CI(ci) => &mut circuits[ci.0].inputs[ci.1],
-        NodeIdx::CO(co) => &mut circuits[co.0].outputs[co.1],
-        NodeIdx::GI(gi) => gate_get_input_mut(circuits, gates, gi),
-        NodeIdx::GO(go) => gate_get_output_mut(circuits, gates, go),
-    }
-}
-
+// update {{{1
 pub(crate) fn update(circuits: &mut CircuitMap, gates: &mut GateMap) {
     // TODO: make this update stack of nodes instead of of gates
     let mut update_stack: Vec<_> = gates.iter().map(|(i, _)| i).collect();
@@ -213,81 +290,5 @@ pub(crate) fn update(circuits: &mut CircuitMap, gates: &mut GateMap) {
                 }
             }
         }
-    }
-}
-
-pub(crate) fn gate_get_input<'a: 'c, 'b: 'c, 'c>(circuits: &'a CircuitMap, gates: &'b GateMap, index: GateInputNodeIdx) -> &'c Node {
-    // let name = gates[index.0].name();
-    let inputs = gate_inputs(circuits, gates, index.0);
-    // inputs.get(index.1).unwrap_or_else(|| panic!("gate input node index invalid: index has index {} but '{}' gate has only {} inputs", index.1, name, inputs.len()))
-    inputs.get(index.1).unwrap()
-}
-pub(crate) fn gate_get_input_mut<'a: 'c, 'b: 'c, 'c>(circuits: &'a mut CircuitMap, gates: &'b mut GateMap, index: GateInputNodeIdx) -> &'c mut Node {
-    // let name = gates[index.0].name();
-    let inputs = gate_inputs_mut(circuits, gates, index.0);
-    // let len = inputs.len();
-    // inputs.get_mut(index.1).unwrap_or_else(|| panic!("gate input node index invalid: index has index {} but '{}' gate has only {} inputs", index.1, name, len))
-    inputs.get_mut(index.1).unwrap()
-    // TODO: there is probably a better way of doing this that doesnt need this code to be copy pasted
-    // TODO: there is also probably a better way of doing this that doesnt need
-}
-pub(crate) fn gate_get_output<'a: 'c, 'b: 'c, 'c>(circuits: &'a CircuitMap, gates: &'b GateMap, index: GateOutputNodeIdx) -> &'c Node {
-    // let name = gates[index.0].name();
-    let outputs = gate_outputs(circuits, gates, index.0);
-    // outputs.get(index.1).unwrap_or_else(|| panic!("gate output node index invalid: index has index {} but '{}' gate has only {} outputs", index.1, name, outputs.len()))
-    outputs.get(index.1).unwrap()
-}
-pub(crate) fn gate_get_output_mut<'a: 'c, 'b: 'c, 'c>(circuits: &'a mut CircuitMap, gates: &'b mut GateMap, index: GateOutputNodeIdx) -> &'c mut Node {
-    // let name = gates[index.0].name();
-    let outputs = gate_outputs_mut(circuits, gates, index.0);
-    // let len = outputs.len();
-    // outputs.get_mut(index.1).unwrap_or_else(|| panic!("gate output node index invalid: index has index {} but '{}' gate has only {} outputs", index.1, name, len))
-    outputs.get_mut(index.1).unwrap()
-}
-
-pub(crate) fn gate_input_indexes(circuits: &CircuitMap, gates: &GateMap, gate: GateIndex) -> impl ExactSizeIterator<Item = GateInputNodeIdx> {
-    (0..gate_num_inputs(circuits, gates, gate)).map(move |i| GateInputNodeIdx(gate, i, ()))
-    // a bit strange because GateIndex is Copy so it really shouldnt have to be moved (?)
-}
-
-pub(crate) fn gate_output_indexes(circuits: &CircuitMap, gates: &GateMap, gate: GateIndex) -> impl ExactSizeIterator<Item = GateOutputNodeIdx> {
-    (0..gate_num_outputs(circuits, gates, gate)).map(move |i| GateOutputNodeIdx(gate, i, ()))
-}
-pub(crate) fn gate_num_inputs(circuits: &CircuitMap, gates: &GateMap, gate: GateIndex) -> usize {
-    gate_inputs(circuits, gates, gate).len()
-}
-pub(crate) fn gate_num_outputs(circuits: &CircuitMap, gates: &GateMap, gate: GateIndex) -> usize {
-    gate_outputs(circuits, gates, gate).len()
-}
-
-pub(crate) fn gate_inputs<'a: 'c, 'b: 'c, 'c>(circuits: &'a CircuitMap, gates: &'b GateMap, gate: GateIndex) -> &'c [Node] {
-    let gate = &gates[gate];
-    match &gate.calculation.kind {
-        CalculationKind::Nand(i, _) => i,
-        CalculationKind::Const(i, _) => i,
-        CalculationKind::Custom(circuit_idx) => &circuits[*circuit_idx].inputs,
-    }
-}
-pub(crate) fn gate_outputs<'a: 'c, 'b: 'c, 'c>(circuits: &'a CircuitMap, gates: &'b GateMap, gate: GateIndex) -> &'c [Node] {
-    let gate = &gates[gate];
-    match &gate.calculation.kind {
-        CalculationKind::Nand(_, o) | CalculationKind::Const(_, o) => o,
-        CalculationKind::Custom(circuit_idx) => &circuits[*circuit_idx].outputs,
-    }
-}
-
-pub(crate) fn gate_inputs_mut<'a: 'c, 'b: 'c, 'c>(circuits: &'a mut CircuitMap, gates: &'b mut GateMap, gate: GateIndex) -> &'c mut [Node] {
-    let gate = &mut gates[gate];
-    match &mut gate.calculation.kind {
-        CalculationKind::Nand(i, _) => i,
-        CalculationKind::Const(i, _) => i,
-        CalculationKind::Custom(circuit_idx) => &mut circuits[*circuit_idx].inputs,
-    }
-}
-pub(crate) fn gate_outputs_mut<'a: 'c, 'b: 'c, 'c>(circuits: &'a mut CircuitMap, gates: &'b mut GateMap, gate: GateIndex) -> &'c mut [Node] {
-    let gate = &mut gates[gate];
-    match &mut gate.calculation.kind {
-        CalculationKind::Nand(_, o) | CalculationKind::Const(_, o) => o,
-        CalculationKind::Custom(circuit_idx) => &mut circuits[*circuit_idx].outputs,
     }
 }
