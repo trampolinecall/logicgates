569eba690c49f63100a37598af4e86460a1008c4
trampolinecall <>
2023-06-11 19:55:32 -0400

    set up elm-esque mvc event loop

--- src/ui.rs
+++ src/ui.rs
@@ -4,10 +4,20 @@ pub(crate) mod gate;
 pub(crate) mod node;
 pub(crate) mod simulation;
 
+// mvc pattern inspired by elm architecture
+pub(crate) struct View {
+    sim: simulation::SimulationWidget
+}
+
 trait Widget {
     fn draw(&self, simulation: &Simulation, draw: &nannou::Draw, rect: nannou::geom::Rect);
 }
 
-pub(crate) fn render(app: &nannou::App, draw: &nannou::Draw, simulation: &Simulation) {
-    // simulation.widget.draw(simulation, draw, app.window_rect())
+pub(crate) fn view(simulation: &Simulation) -> View {
+    todo!()
+}
+
+pub(crate) fn render(app: &nannou::App, draw: &nannou::Draw, view: &View) {
+    todo!()
+    // view.sim.draw(view, draw, app.window_rect());
 }
