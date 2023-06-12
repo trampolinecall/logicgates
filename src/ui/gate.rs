48f952152918aaa27824c5ae80eb2c7e84cac42f
trampolinecall <>
2023-06-12 07:02:23 -0400

    add locations to widgets

--- src/ui/gate.rs
+++ src/ui/gate.rs
@@ -9,13 +9,14 @@ use crate::{
 
 pub(crate) struct GateWidget {
     pub(crate) key: GateKey,
+    pub(crate) rect: nannou::geom::Rect,
 }
 
 const GATE_COLOR: Rgb = Rgb { red: 0.584, green: 0.647, blue: 0.65, standard: PhantomData };
 
 impl Widget for GateWidget {
-    fn draw(&self, simulation: &Simulation, draw: &nannou::Draw, rect: nannou::geom::Rect) {
-        draw.rect().color(GATE_COLOR).xy(rect.xy()).wh(rect.wh());
-        draw.text(simulation.gates[self.key].name(&simulation.circuits)).xy(rect.xy()).wh(rect.wh()).center_justify().align_text_middle_y();
+    fn draw(&self, simulation: &Simulation, draw: &nannou::Draw) {
+        draw.rect().color(GATE_COLOR).xy(self.rect.xy()).wh(self.rect.wh());
+        draw.text(simulation.gates[self.key].name(&simulation.circuits)).xy(self.rect.xy()).wh(self.rect.wh()).center_justify().align_text_middle_y();
     }
 }
--- src/ui/node.rs
+++ src/ui/node.rs
@@ -9,7 +9,11 @@ use crate::{
 
 pub(crate) struct NodeWidget {
     pub(crate) key: NodeKey,
+    pub(crate) location: nannou::geom::Vec2,
 }
+
+// TODO: refactor so that this doesnt need this to be pub(crate)
+pub(crate) const CIRCLE_RAD: f32 = 5.0;
 
 const ON_COLOR: Rgb = Rgb { red: 0.18, green: 0.8, blue: 0.521, standard: PhantomData };
 const OFF_COLOR: Rgb = Rgb { red: 0.498, green: 0.549, blue: 0.552, standard: PhantomData };
@@ -17,9 +21,9 @@ const HIGH_IMPEDANCE_COLOR: Rgb = Rgb { red: 52.0 / 255.0, green: 152.0 / 255.0,
 const ERR_COLOR: Rgb = Rgb { red: 231.0 / 255.0, green: 76.0 / 255.0, blue: 60.0 / 255.0, standard: PhantomData };
 
 impl Widget for NodeWidget {
-    fn draw(&self, simulation: &Simulation, draw: &nannou::Draw, rect: nannou::geom::Rect) {
+    fn draw(&self, simulation: &Simulation, draw: &nannou::Draw) {
         let color = node_color(&simulation.nodes, self.key, true);
-        draw.ellipse().color(color).xy(rect.xy()).radius(rect.w() / 2.0);
+        draw.ellipse().color(color).xy(self.location).radius(CIRCLE_RAD);
     }
 }
 
