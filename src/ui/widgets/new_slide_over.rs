d5266fd6db4888b3b1cb133dbcb2e05560a71f60
Jason N <fireworkinyourbathtub@gmail.com>
2023-06-20 12:18:22 -0400

    clean up new slide over view

--- src/ui/widgets/new_slide_over.rs
+++ src/ui/widgets/new_slide_over.rs
@@ -8,11 +8,6 @@ use crate::{
         Event, Subscription, View,
     },
     theme::Theme,
-    ui::{
-        message::{TargetedUIMessage, UIMessage},
-        widgets::WidgetId,
-    },
-    view,
 };
 
 // TODO: implement slide over from other sides
@@ -27,25 +22,9 @@ impl SlideOverState {
     pub(crate) fn new() -> Self {
         Self { drawer_out: false, last_switch_time: Duration::ZERO, toggle_button: ButtonState::new() }
     }
-
-    /*
-    fn calculate_slide_over_rects(&self, app: &nannou::App, rect: nannou::geom::Rect) -> (Option<nannou::geom::Rect>, nannou::geom::Rect) {
-        let time_since_switch = app.duration.since_start - self.last_switch_time;
-        let time_interp = (Theme::DEFAULT.animation_ease)((time_since_switch.as_secs_f32() / Theme::DEFAULT.animation_time).clamp(0.0, 1.0));
-        let x_interp = if self.drawer_out { time_interp } else { 1.0 - time_interp };
-
-        let over_size = self.over.size(rect.w_h());
-        let over_rect = nannou::geom::Rect::from_wh(over_size.into()).align_y_of(nannou::geom::Align::End, rect).left_of(rect).shift_x(over_size.0 * x_interp);
-        let toggle_button_rect = nannou::geom::Rect::from_wh(Theme::DEFAULT.slide_out_size.into()).right_of(over_rect).align_top_of(rect).shift_y(-Theme::DEFAULT.slide_out_toggle_y_offset);
-        let over_rect_needed = self.drawer_out || time_interp != 1.0; // drawer is out or we are in the middle of an animation
-        (if over_rect_needed { Some(over_rect) } else { None }, toggle_button_rect)
-    }
-    */
 }
 
-struct SlideOverView<Data, L: Lens<Data, SlideOverState>, ButtonView: View<Data>, BaseView: View<Data>, OverView: View<Data>> {
-    lens: L,
-
+struct SlideOverView<Data, ButtonView: View<Data>, BaseView: View<Data>, OverView: View<Data>> {
     base: BaseView,
     over: OverView,
     button: ButtonView,
@@ -62,7 +41,7 @@ struct SlideOverLayout {
     toggle_button_rect: nannou::geom::Rect,
 }
 
-impl<Data, L: Lens<Data, SlideOverState>, ButtonView: View<Data>, BaseView: View<Data>, OverView: View<Data>> SlideOverView<Data, L, ButtonView, BaseView, OverView> {
+impl<Data, ButtonView: View<Data>, BaseView: View<Data>, OverView: View<Data>> SlideOverView<Data, ButtonView, BaseView, OverView> {
     fn layout<'layout>(&self, given_rect: nannou::geom::Rect, layout_field: &'layout mut Option<(nannou::geom::Rect, SlideOverLayout)>) -> &'layout SlideOverLayout {
         // the layout should really never need to be computed more than once because the view tree is supposed to only be used for one frame so the layout should really never change
         // but conceptually the layout is supposed to be computed for whatever rect is passed into draw() or find_hover() and this only caches whatever layout was computed last
@@ -86,7 +65,7 @@ impl<Data, L: Lens<Data, SlideOverState>, ButtonView: View<Data>, BaseView: View
     }
 }
 
-impl<Data, L: Lens<Data, SlideOverState>, ButtonView: View<Data>, BaseView: View<Data>, OverView: View<Data>> View<Data> for SlideOverView<Data, L, ButtonView, BaseView, OverView> {
+impl<Data, ButtonView: View<Data>, BaseView: View<Data>, OverView: View<Data>> View<Data> for SlideOverView<Data, ButtonView, BaseView, OverView> {
     fn draw(&self, app: &nannou::App, draw: &nannou::Draw, rect: nannou::geom::Rect, hover: Option<ViewId>) {
         let mut layout_borrow = self.layout.borrow_mut();
         let layout = self.layout(rect, &mut layout_borrow);
@@ -131,7 +110,7 @@ impl<Data, L: Lens<Data, SlideOverState>, ButtonView: View<Data>, BaseView: View
         self.base.targeted_event(app, data, target, event);
     }
 
-    fn event(&self, app: &nannou::App, data: &mut Data, event: Event) {
+    fn event(&self, _: &nannou::App, _: &mut Data, event: Event) {
         match event {
             Event::LeftMouseDown => {}
         }
@@ -166,66 +145,5 @@ pub(crate) fn slide_over<Data>(
             slide_over_data.last_switch_time = app.duration.since_start;
         },
     );
-    SlideOverView { lens: get_slide_over_data, base, over, button, drawer_openness, layout: RefCell::new(None), _phantom: PhantomData }
-}
-
-struct ToggleButtonDrawing {
-    slide_over_id: WidgetId,
-    rect: nannou::geom::Rect,
-    pressed: bool,
-}
-
-struct SlideOverDrawing {
-    base_drawing: Box<dyn view::Drawing>,
-    over_drawing: Option<Box<dyn view::Drawing>>,
-    toggle_button_drawing: ToggleButtonDrawing,
-}
-impl view::Drawing for SlideOverDrawing {
-    fn draw(&self, logic_gates: &crate::LogicGates, draw: &nannou::Draw, hovered: Option<&dyn view::Drawing>) {
-        self.base_drawing.draw(logic_gates, draw, hovered);
-        if let Some(over_drawing) = &self.over_drawing {
-            over_drawing.draw(logic_gates, draw, hovered)
-        }
-        self.toggle_button_drawing.draw(logic_gates, draw, hovered);
-    }
-
-    fn find_hover(&self, mouse: nannou::prelude::Vec2) -> Option<&dyn view::Drawing> {
-        if let x @ Some(_) = self.toggle_button_drawing.find_hover(mouse) {
-            return x;
-        }
-        if let Some(over_drawing) = &self.over_drawing {
-            if let x @ Some(_) = over_drawing.find_hover(mouse) {
-                return x;
-            }
-        }
-
-        self.base_drawing.find_hover(mouse)
-    }
-}
-impl view::Drawing for ToggleButtonDrawing {
-    fn draw(&self, _: &crate::LogicGates, draw: &nannou::Draw, hovered: Option<&dyn view::Drawing>) {
-        let mut rect = draw.rect().xy(self.rect.xy()).wh(self.rect.wh()).color(Theme::DEFAULT.button_normal_bg);
-        if let Some(hovered) = hovered {
-            if std::ptr::eq(hovered, self) {
-                // TODO: fix clippy lint about this
-                rect = rect.color(Theme::DEFAULT.button_hover_bg);
-            }
-        }
-        if self.pressed {
-            rect = rect.color(Theme::DEFAULT.button_pressed_bg);
-        }
-        rect.finish()
-    }
-
-    fn find_hover(&self, mouse: nannou::prelude::Vec2) -> Option<&dyn view::Drawing> {
-        if self.rect.contains(mouse) {
-            Some(self)
-        } else {
-            None
-        }
-    }
-
-    fn left_mouse_down(&self, _: &nannou::App) -> Option<TargetedUIMessage> {
-        Some(TargetedUIMessage { target: self.slide_over_id, message: UIMessage::MouseDownOnSlideOverToggleButton })
-    }
+    SlideOverView { base, over, button, drawer_openness, layout: RefCell::new(None), _phantom: PhantomData }
 }
