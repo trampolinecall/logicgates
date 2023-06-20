f3d3e871c14559042b0f3ca14a46422643501d27
Jason N <fireworkinyourbathtub@gmail.com>
2023-06-20 15:10:07 -0400

    add new simulation widget

--- src/ui/widgets.rs
+++ src/ui/widgets.rs
@@ -5,6 +5,7 @@ pub(crate) mod slider;
 pub(crate) mod test_rect;
 
 pub(crate) mod new_flow;
+pub(crate) mod new_simulation;
 pub(crate) mod new_slide_over;
 pub(crate) mod new_slider;
 pub(crate) mod new_test_rect;
