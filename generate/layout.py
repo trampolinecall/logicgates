f5475ec104d96bf2469f9aa37066bcd8cd2fdc14
trampolinecall <>
2023-06-27 21:02:23 -0400

    add different directions of flow layout

--- generate/layout.py
+++ generate/layout.py
@@ -16,24 +16,68 @@ class Gate:
     def apply(self, center=(0, 0)):
         self.gate.layout = GateLayout(center, self.direction)
 
-class HorizontalFlow:
-    def __init__(self, *children):
+class Flow:
+    def __init__(self, direction, *children):
+        self.direction = direction
         self.children = children
 
     def size(self):
         children_sizes = [child.size() for child in self.children]
-        width = sum([size[0] for size in children_sizes])
-        height = max(children_sizes, key=lambda size: size[1])
+        children_widths = [size[0] for size in children_sizes]
+        children_heights = [size[1] for size in children_sizes]
+
+        match self.direction:
+            case 'ltr' | 'rtl':
+                width = sum(children_widths)
+                height = max(children_heights)
+            case 'ttb' | 'btt':
+                width = max(children_widths)
+                height = sum(children_heights)
+
+            case _:
+                raise Exception(f'invalid flow direction \'{self.direction}\'')
+
         return (width, height)
 
     def apply(self, center=(0, 0)):
         center_x, center_y = center
         size_x, size_y = self.size()
 
-        cur_x = center_x - size_x / 2
+        match self.direction:
+            case 'ltr':
+                cur_pos = center_x - size_x / 2
+            case 'rtl':
+                cur_pos = center_x + size_x / 2
+            case 'ttb':
+                cur_pos = center_y - size_y / 2
+            case 'btt':
+                cur_pos = center_y + size_y / 2
+
         for child in self.children:
-            child.apply((cur_x, center_y))
-            cur_x += child.size()[0]
+            match self.direction:
+                case 'ltr' | 'rtl':
+                    child.apply((cur_pos, center_y))
+                case 'ttb' | 'btt':
+                    child.apply((center_x, cur_pos))
+
+            match self.direction:
+                case 'ltr':
+                    cur_pos += child.size()[0]
+                case 'rtl':
+                    cur_pos -= child.size()[0]
+                case 'ttb':
+                    cur_pos += child.size()[1]
+                case 'btt':
+                    cur_pos -= child.size()[1]
+
+def ltr_flow(*children):
+    return Flow('ltr', *children)
+def rtl_flow(*children):
+    return Flow('rtl', *children)
+def ttb_flow(*children):
+    return Flow('ttb', *children)
+def btt_flow(*children):
+    return Flow('btt', *children)
 
 def ltr_gate(gate):
     return Gate(gate, 'ltr')
