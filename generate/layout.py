9fd773c2bf9c781b7873f68dce25f118b8bbab7a
trampolinecall <>
2023-06-27 20:49:53 -0400

    change layout

--- generate/layout.py
+++ generate/layout.py
@@ -5,29 +5,41 @@ class GateLayout:
         self.position = position
         self.direction = direction
 
-# TODO:
-# class Gate:
-#     def __init__(self, gate):
-#         self.gate = gate
-#
-#     def size(self):
-#         return INDIVIDUAL_GATE_RECT
-#
-#     def apply(self, center=(0, 0)):
-#         self.gate.layout = GateLayout(center, )
-
-class LTRChain:
-    def __init__(self, *gates):
-        self.gates = gates
+class Gate:
+    def __init__(self, gate, direction):
+        self.gate = gate
+        self.direction = direction
 
     def size(self):
-        return (INDIVIDUAL_GATE_RECT[0] * len(self.gates), INDIVIDUAL_GATE_RECT[1])
+        return INDIVIDUAL_GATE_RECT
+
+    def apply(self, center=(0, 0)):
+        self.gate.layout = GateLayout(center, self.direction)
+
+class HorizontalFlow:
+    def __init__(self, *children):
+        self.children = children
+
+    def size(self):
+        children_sizes = [child.size() for child in self.children]
+        width = sum([size[0] for size in children_sizes])
+        height = max(children_sizes, key=lambda size: size[1])
+        return (width, height)
 
     def apply(self, center=(0, 0)):
         center_x, center_y = center
         size_x, size_y = self.size()
 
         cur_x = center_x - size_x / 2
-        for child in self.gates:
-            child.layout = GateLayout((cur_x, center_y), 'ltr')
-            cur_x += INDIVIDUAL_GATE_RECT[0]
+        for child in self.children:
+            child.apply((cur_x, center_y))
+            cur_x += child.size()[0]
+
+def ltr_gate(gate):
+    return Gate(gate, 'ltr')
+def rtl_gate(gate):
+    return Gate(gate, 'rtl')
+def ttb_gate(gate):
+    return Gate(gate, 'ttb')
+def btt_gate(gate):
+    return Gate(gate, 'btt')
